2afcaa5ad4b09e356bd500e05766cd862b6d361f
JonFreer <j.freer@hotmail.co.uk>
2023-05-24 13:20:05 +0100

    Init 2

---
+++ api.py
@@ -0,0 +1,138 @@
+import requests
+import datetime
+import json
+
+class API:
+
+    # Call and return the json from the Vivacity API
+    def get_results(api_key,start_time, end_time):
+        response = requests.get("https://tfwm.onl/vivacity.json?ApiKey={}&earliest={}&latest={}&name=L48_Bristol_rd_cyclepath_wm5g001&class=cyclist&NullDataPoints=false&to=1684840206".format(api_key,start_time,end_time))
+        response.raise_for_status()
+        return response.json()
+
+
+    def filter_results(results: dict):
+
+        out = {}
+        records = results.get("Vivacity", {}).get("kids", {})
+        
+        # Iterate over values and filter out any where the Start, Centre and End values are None
+        # Store the output for a given time in a dict
+        for value in records.values():
+            data = value["kids"]
+            if not data:
+                continue
+            location = data.get("Location", {}).get("kids", {})
+            valid_location = location and location.get("Start") is not None
+            valid_location = valid_location and location.get("Centre") is not None
+            valid_location = valid_location and location.get("End") is not None
+
+            if not valid_location:
+                continue
+
+            date_string = data.get("Dates", {}).get("kids", {}).get("From")
+
+            count = data.get("Counts", {}).get("kids", {})
+
+            date_time = datetime.datetime.strptime(date_string, '%Y-%m-%d %H:%M:%S') # Get Unix time
+
+            out[date_time] = count
+
+        return out
+
+
+    # def aggregate_results(results: Iterable[dict]) -> dict:
+
+    #     totals = defaultdict(dict)
+
+    #     for result in results:
+    #         name = result.get("Name")
+    #         counts = result.get("Counts", {}).get("kids", {})
+    #         if not name or not counts:
+    #             continue
+
+    #         for key, value in counts.items():
+    #             value = (value or "").strip()
+    #             if not value.isdigit():
+    #                 raise ValueError(value)
+    #             value = int(value)
+    #             totals[name][key] = totals[name].get(key, 0) + value
+
+    #     return totals
+
+
+    # def get_last_hour():
+    #     in_count, out_count = 0, 0
+    #     t = str(int(time.time()) - 3600)
+    #     all_results = get_results(t)
+    #     filtered_results = filter_results(all_results)
+    #     aggregates = aggregate_results(filtered_results)
+    #     for name, counts in aggregates.items():
+    #         print(f"{name} {counts.get('In', 0)} in {counts.get('Out', 0)} out")
+    #         in_count, out_count = counts.get('In', 0), counts.get('Out', 0)
+    #     return in_count, out_count
+
+    def aggregate_hourly(data):
+        hourly_total = []
+        for i in range(24):
+            hourly_total.append({"In": 0, "Out": 0})
+
+        for key, item in data.items():
+            hourly_total[key.hour]["In"] = hourly_total[key.hour]["In"] + \
+                int(item["In"])
+            hourly_total[key.hour]["Out"] = hourly_total[key.hour]["Out"] + \
+                int(item["Out"])
+        return hourly_total
+
+    def get_total(data):
+        in_count, out_count = 0, 0
+        for key, item in data.items():
+            in_count = in_count + int(item["In"])
+            out_count = out_count + int(item["Out"])
+        return in_count, out_count
+    
+    # Output the data to json and js
+    def output_data(data,date):
+        with open('hourly.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+
+        hourly_json = json.dumps(data)
+
+        with open('hourly.js', 'w', encoding='utf-8') as f:
+            f.write("hourly_data = ")
+            f.write(hourly_json)
+            f.write("\nday = ")
+            f.write(date)
+
+
+
+    # Get todays daily total
+    def get_today(api_key):
+
+        all_results = API.get_results(api_key,"today", "now")
+        filtered_results = API.filter_results(all_results)
+        hourly_total = API.aggregate_hourly(filtered_results)
+     
+        API.output_data(hourly_total,str(int(datetime.datetime.now().timestamp())))
+
+        in_count, out_count = API.get_total(filtered_results)
+
+        return in_count, out_count
+
+
+    def get_yesterday(api_key):
+
+        # Get the start time and end time as unix timestamp
+        now = datetime.datetime.now()
+        end = int(now.replace(hour=0, minute=0, second=0, microsecond=0).timestamp())
+        start = end - (24*60*60)
+
+        all_results = API.get_results(api_key,start, end)
+        filtered_results = API.filter_results(all_results)
+        hourly_total = API.aggregate_hourly(filtered_results)
+
+        API.output_data(hourly_total,str(int(datetime.datetime.now().timestamp()) - (24*60*60)))
+
+        in_count, out_count = API.get_total(filtered_results)
+
+        return in_count, out_count
---
+++ cycle_tweeter.py
@@ -0,0 +1,104 @@
+#!/usr/bin/env python
+
+# nohup python3 cycle_tweeter.py &
+# ps ax | grep cycle_tweeter.py
+
+from collections import defaultdict
+from typing import Generator, Iterable
+from multiprocessing import current_process
+from dotenv import load_dotenv
+from api import API
+from twitter_media import TwitterMedia
+
+import logging
+import time
+import os
+import schedule
+import tweepy
+
+from requests_oauthlib import OAuth1
+
+def job():
+    try:
+        LOGGER.info("Calling API")
+        day_in, day_out = API.get_yesterday(os.environ["VIVACITY_KEY"])
+        LOGGER.info("Called API. Yesterday totals are {} and {}".format(day_in, day_out))
+
+        # os.system("chromium --headless --disable-gpu --screenshot ./index.html --force-device-scale-factor=2 --window-size=1060,590")
+
+        time.sleep(10)
+
+        post_on_twitter(day_in, day_out)
+
+    except Exception as e:
+        # print(e)
+        # f = open("cycle_log.txt", "a")
+        # f.write("\nNew Error")
+        # f.write(str(e))
+        # f.close()
+        LOGGER.error(str(e))
+
+
+def post_on_twitter(day_in, day_out):
+
+    FILENAME = 'screenshot.png'
+    MEDIA_CATEGORY = 'tweet_image'
+
+    twitterMedia = TwitterMedia(FILENAME, MEDIA_CATEGORY,oauth)
+    twitterMedia.upload_init()
+    twitterMedia.upload_append()
+    twitterMedia.upload_finalize()
+
+    tweet = "Yesterday, there were {} people cycling on the ~A38Cycleway. {} headed into the city, {} headed towards Selly Oak.".format(day_in + day_out,day_in,day_out)
+
+    response = client.create_tweet(text=tweet, media_ids=[twitterMedia.media_id])
+
+
+if __name__ == '__main__':
+
+    ### Set up logging
+    # Create logger with 'spam_application'
+    LOGGER = logging.getLogger()
+    LOGGER.setLevel(logging.DEBUG)
+    formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s',
+                                  datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        level=logging.INFO,
+        datefmt='%Y-%m-%d %H:%M:%S')
+    
+    # create file handler which logs even debug messages
+    fh = logging.FileHandler('cycle_tweet.log')
+    fh.setFormatter(formatter)
+    fh.setLevel(logging.DEBUG)
+    LOGGER.addHandler(fh)
+    LOGGER.info('Starting Bot. PID: {}'.format(current_process().pid))
+
+    # Load .env file with api keys
+    load_dotenv(override=True)
+
+    # Set up OAuth and Tweepy Client for tweeting
+    client = tweepy.Client(
+        consumer_key=os.environ["CONSUMER_KEY"], consumer_secret=os.environ["CONSUMER_SECRET"],
+        access_token=os.environ["ACCESS_TOKEN"], access_token_secret=os.environ["ACCESS_TOKEN_SECRET"]
+    )
+
+    oauth = OAuth1(os.environ["CONSUMER_KEY"],
+                   client_secret=os.environ["CONSUMER_SECRET"],
+                   resource_owner_key=os.environ["ACCESS_TOKEN"],
+                   resource_owner_secret=os.environ["ACCESS_TOKEN_SECRET"])
+    
+
+    # print( os.environ["CONSUMER_KEY"],
+    #                os.environ["CONSUMER_SECRET"],
+    #                os.environ["ACCESS_TOKEN"],
+    #                os.environ["ACCESS_TOKEN_SECRET"])
+    job()
+
+    # # Schedule the job to run once an hour
+    # schedule.every().hour.at(":01").do(job)
+
+    # # Loop to run job
+    # while True:
+    #     schedule.run_pending()
+    #     time.sleep(10)
